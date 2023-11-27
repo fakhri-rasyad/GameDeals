ffb02cc9a121985eaa38c00496b899e8a2b11c15
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-11-27 20:41:39 +0800

    Penambahan fungsi IconAndDetail pada GameSearchScreen

--- app/src/main/java/com/d121211017/gamedeales/ui/screen/GameSearchScreen.kt
+++ app/src/main/java/com/d121211017/gamedeales/ui/screen/GameSearchScreen.kt
@@ -1,23 +1,34 @@
 package com.d121211017.gamedeales.ui.screen
 
+import android.graphics.drawable.Icon
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.d121211017.gamedeales.R
 import com.d121211017.gamedeales.ui.theme.GameDealesTheme
 
 @Composable
@@ -33,7 +44,9 @@ fun GameSearchBar(){
         TextField(
             value = "",
             onValueChange = {},
-            modifier = Modifier.height(48.dp).weight(2f))
+            modifier = Modifier
+                .height(48.dp)
+                .weight(2f))
         Button(
             onClick = { /*TODO*/ },
             modifier = Modifier.height(48.dp),
@@ -41,6 +54,39 @@ fun GameSearchBar(){
         {
             Icon(imageVector = Icons.Default.Search, contentDescription = "")
         }
+    }
+    IconAndDetail(
+        image = R.drawable.search_fill1_wght400_grad0_opsz24,
+        description = "Ayo Cari Game Anda!",
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 32.dp)
+        )
+    IconAndDetail(
+        image = R.drawable.help_fill1_wght400_grad0_opsz24,
+        description = "Sepertinya Game Yang Anda Cari Tidak Ada!",
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 32.dp)
+    )
+    IconAndDetail(
+        image = R.drawable.warning_fill1_wght400_grad0_opsz24,
+        description = "Sepertinya Jaringan Anda Sedang Bermasalah Silahkan Coba Lagi Nanti",
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp)
+    )
+}
+
+@Composable
+fun IconAndDetail(@DrawableRes image: Int, description: String, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+        ) {
+        Image(painterResource(id = image), contentDescription = "", Modifier.size(128.dp))
+        Text(description, textAlign = TextAlign.Center, lineHeight = 28.sp , fontSize = 28.sp)
     }
 }
 
