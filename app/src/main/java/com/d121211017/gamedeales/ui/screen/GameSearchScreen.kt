f1f9ac4e22e48592a563d5c0d7a7815684662873
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-11-28 13:55:55 +0800

    Pembuatan komponen grid untuk kartu detail game

--- app/src/main/java/com/d121211017/gamedeales/ui/screen/GameSearchScreen.kt
+++ app/src/main/java/com/d121211017/gamedeales/ui/screen/GameSearchScreen.kt
@@ -12,11 +12,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -36,27 +38,28 @@ import com.d121211017.gamedeales.ui.theme.GameDealesTheme
 fun GameSearchScreen(modifier: Modifier = Modifier){
     Column(modifier){
         GameSearchBar()
-        //    IconAndDetail(
-//        image = R.drawable.search_fill1_wght400_grad0_opsz24,
-//        description = "Ayo Cari Game Anda!",
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .padding(top = 64.dp)
+        GameDisplayCard(gameThumbnail = R.drawable.help_fill1_wght400_grad0_opsz24, gameName = "Game Name Placeholder")
+//        //    IconAndDetail(
+////        image = R.drawable.search_fill1_wght400_grad0_opsz24,
+////        description = "Ayo Cari Game Anda!",
+////        modifier = Modifier
+////            .fillMaxWidth()
+////            .padding(top = 64.dp)
+////        )
+//        IconAndDetail(
+//            image = R.drawable.help_fill1_wght400_grad0_opsz24,
+//            description = "Sepertinya Game Yang Anda Cari Tidak Ada!",
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(top = 32.dp)
 //        )
-        IconAndDetail(
-            image = R.drawable.help_fill1_wght400_grad0_opsz24,
-            description = "Sepertinya Game Yang Anda Cari Tidak Ada!",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 32.dp)
-        )
-//    IconAndDetail(
-//        image = R.drawable.warning_fill1_wght400_grad0_opsz24,
-//        description = "Sepertinya Jaringan Anda Sedang Bermasalah Silahkan Coba Lagi Nanti",
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .padding(top = 32.dp)
-//    )
+////    IconAndDetail(
+////        image = R.drawable.warning_fill1_wght400_grad0_opsz24,
+////        description = "Sepertinya Jaringan Anda Sedang Bermasalah Silahkan Coba Lagi Nanti",
+////        modifier = Modifier
+////            .fillMaxSize()
+////            .padding(top = 32.dp)
+////    )
     }
 }
 
@@ -77,6 +80,7 @@ fun GameSearchBar(){
             Icon(imageVector = Icons.Default.Search, contentDescription = "")
         }
     }
+    Spacer(Modifier.height(64.dp))
 
 }
 
@@ -87,14 +91,27 @@ fun IconAndDetail(@DrawableRes image: Int, description: String, modifier: Modifi
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
         ) {
-        Spacer(Modifier.height(64.dp))
         Image(painterResource(id = image), contentDescription = "", Modifier.size(128.dp))
         Spacer(Modifier.height(16.dp))
         Text(description, textAlign = TextAlign.Center, lineHeight = 28.sp , fontSize = 28.sp)
     }
 }
 @Composable
-fun GameDisplayCard(){
+fun GameDisplayCard(gameThumbnail: Int, gameName: String){
+    Card(){
+        Column(
+            Modifier
+                .weight(.4f)
+                .height(96.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(painterResource(id = gameThumbnail), contentDescription = gameName)
+            Text(gameName)
+        }
+    }
+}
+
+@Composable
+fun GameDisplayGrid(){
 
 }
 
