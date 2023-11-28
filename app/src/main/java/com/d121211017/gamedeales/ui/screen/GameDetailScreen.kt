09e3aeab84fbe94cfa0efba3a181ed1b04d3caad
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-11-28 19:14:59 +0800

    Penambahan komponen GameDealCard

--- app/src/main/java/com/d121211017/gamedeales/ui/screen/GameDetailScreen.kt
+++ app/src/main/java/com/d121211017/gamedeales/ui/screen/GameDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.d121211017.gamedeales.ui.screen
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.d121211017.gamedeales.ui.theme.GameDealesTheme
@@ -11,6 +12,11 @@ fun GameDetailScreen(){
 
 @Composable
 fun GameDetailHeader(){
+
+}
+
+@Composable
+fun GameDealCard(){
 
 }
 
