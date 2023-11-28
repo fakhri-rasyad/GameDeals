36ace10da489e8dc6c86ff6a9e000a364140596f
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2023-11-28 16:19:15 +0800

    Penambahan Komponen App Bar

--- app/src/main/java/com/d121211017/gamedeales/ui/GameDealScreen.kt
+++ app/src/main/java/com/d121211017/gamedeales/ui/GameDealScreen.kt
@@ -1,4 +1,48 @@
 package com.d121211017.gamedeales.ui
 
-class GameDealScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.d121211017.gamedeales.ui.screen.GameSearchScreen
+import com.d121211017.gamedeales.ui.theme.GameDealesTheme
+
+@Composable
+fun GameDealApp(){
+    Scaffold(
+        topBar = { GameDealAppBar(Modifier.padding(16.dp)) }
+    ) {innerPadding ->
+        Column(Modifier.padding(innerPadding).padding(16.dp)) {
+            GameSearchScreen()
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GameDealAppBar(modifier: Modifier = Modifier){
+    TopAppBar(
+        title = { Text("Deal Me Some Games", textAlign = TextAlign.Center)},
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        )
+}
+
+@Preview
+@Composable
+fun GameDealScreenPreview(){
+    GameDealesTheme {
+        GameDealApp()
+    }
 }
